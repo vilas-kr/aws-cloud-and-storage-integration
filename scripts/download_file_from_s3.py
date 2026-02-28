55d4afd0d0bd0cd294483048d96d842adc58a0e1
vilas <vilaskr2962@gmail.com>
2026-02-28 10:17:12 +0530

    Updated Program : [ Vilas ] added doc string to functions

--- scripts/download_file_from_s3.py
+++ scripts/download_file_from_s3.py
@@ -3,9 +3,22 @@ import os
 import hashlib
 
 from botocore.exceptions import ClientError
+from upload_file_to_s3 import calculate_md5
 
 def download_file_from_s3(bucket_name, s3_key, download_dir):
-    """Downloads a file from an S3 bucket."""
+    """ Download a file from an S3 bucket and save it to a specified 
+        directory.
+
+    Args:
+        bucket_name (str): The name of the S3 bucket.
+        s3_key (str): The key of the file in the S3 bucket.
+        download_dir (str): The local directory where the file will be 
+            downloaded.
+
+    Raises:
+        ClientError: If there is an error with the AWS S3 client.
+        Exception: For any other unexpected errors.
+    """
     try:
         os.makedirs(download_dir, exist_ok=True)
         download_path = os.path.join(download_dir, s3_key)
@@ -16,79 +29,89 @@ def download_file_from_s3(bucket_name, s3_key, download_dir):
             s3_client.head_bucket(Bucket=bucket_name)
         except ClientError as e:
             error_code = e.response['Error']['Code']
-            if  error_code in ["404", "NoSuchBucket"]:
-                print("Bucket does not exist.")
+            if  error_code in ['404', 'NoSuchBucket']:
+                print('Bucket does not exist.')
                 return
-            elif error_code in ["403", "AccessDenied"]:
-                print("Access denied to bucket.")
+            elif error_code in ['403', 'AccessDenied']:
+                print('Access denied to bucket.')
                 return
             else:
                 raise e
 
         try:        
-            print(f"Downloading {s3_key} from bucket {bucket_name}...")
+            print(f'Downloading {s3_key} from bucket {bucket_name}...')
             s3_client.download_file(bucket_name, s3_key, download_path)
-            print("File downloaded successfully")
+            print('File downloaded successfully')
         except ClientError as e:
             error_code = e.response['Error']['Code']
-            if error_code in ["404", "NoSuchKey"]:
-                print("File does not exist in S3 bucket.")
+            if error_code in ['404', 'NoSuchKey']:
+                print('File does not exist in S3 bucket.')
             else:
                 raise e
 
     except Exception as e:
-        print(f"Unexpected error: {e}")
+        print(f'Unexpected error: {e}')
 
-def calculate_md5(file_path):
-    """Calculate MD5"""
-    hash_md5 = hashlib.md5()
 
-    with open(file_path, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
-            hash_md5.update(chunk)
-
-    return hash_md5.hexdigest()
 
 def check__integrity(file_path, bucket_name, s3_key):
-    """Check integrity of the downloaded file by comparing MD5 hash."""
+    """Check integrity of the downloaded file by comparing MD5 hash.
+    Args:
+        file_path (str): The path to the downloaded file.
+        bucket_name (str): The name of the S3 bucket.
+        s3_key (str): The key of the file in the S3 bucket.
+
+    Raises:
+        ClientError: If there is an error retrieving metadata from S3.
+        KeyError: If the MD5 metadata is not found in the S3 object.
+        Exception: For any other unexpected errors.
+    """
     local_md5 = calculate_md5(file_path)
     
     s3_client = boto3.client('s3')
     response = s3_client.head_object(Bucket=bucket_name, Key=s3_key)
-    remote_md5 = response["Metadata"]["md5"]
+    remote_md5 = response['Metadata']['md5']
     
     if local_md5 == remote_md5:
-        print("file integrity verified successfully.")
+        print('file integrity verified successfully.')
     else:
-        print("file integrity verification failed. The downloaded file may be corrupted")
+        print('file integrity verification failed. The downloaded file may be corrupted')
 
         
 def list_files_in_s3_bucket(bucket_name):
-    """Lists files in an S3 bucket."""
+    """List files in an S3 bucket.
+
+    Args:
+        bucket_name (str): The name of the S3 bucket.
+        
+    Raises:
+        ClientError: If there is an error with the AWS S3 client.
+        Exception: For any other unexpected errors.
+    """
     try:
         s3_client = boto3.client('s3')
-        paginator = s3_client.get_paginator("list_objects_v2")
+        paginator = s3_client.get_paginator('list_objects_v2')
 
         found = False
         for page in paginator.paginate(Bucket=bucket_name):
-            for obj in page.get("Contents", []):
+            for obj in page.get('Contents', []):
                 found = True
-                print(f'{obj["Key"]} \t {obj["Size"]} bytes \t Last Modified: {obj["LastModified"]}')
+                print(f'{obj['Key']} \t {obj['Size']} bytes \t Last Modified: {obj['LastModified']}')
         
         if not found:
             print(f'Bucket is empty.')
             
     except ClientError as e:
         error_code = e.response['Error']['Code']
-        if error_code in ["404", "NoSuchBucket"]:
-            print("Bucket does not exist.")
-        elif error_code in ["403", "AccessDenied"]:
-            print("Access denied to bucket.")
+        if error_code in ['404', 'NoSuchBucket']:
+            print('Bucket does not exist.')
+        elif error_code in ['403', 'AccessDenied']:
+            print('Access denied to bucket.')
         else:
-            print(f"Client error: {e}")
+            print(f'Client error: {e}')
             
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f'An error occurred: {e}')
         
 def main():
     bucket_name = 'vilas-aws-storage-project'
--- scripts/upload_file_to_s3.py
+++ scripts/upload_file_to_s3.py
@@ -1,13 +1,22 @@
 import boto3
 import hashlib
+
 from boto3.exceptions import S3UploadFailedError
 from botocore.exceptions import ClientError
 
 def upload_file_to_s3(file_path, bucket_name, s3_key):
+    """upload a file to an S3 bucket and store its MD5 hash in metadata for
+        integrity check.
+
+    Args:
+        file_path (str): The path to the file to be uploaded.
+        bucket_name (str): The name of the S3 bucket to upload the file to.
+        s3_key (str): The key under which the file will be stored in S3.
+    """
     try:
         s3_client = boto3.client('s3')
 
-        print(f"Uploading {file_path} to bucket {bucket_name}...")
+        print(f'Uploading {file_path} to bucket {bucket_name}...')
 
         local_md5 = calculate_md5(file_path)
 
@@ -16,32 +25,39 @@ def upload_file_to_s3(file_path, bucket_name, s3_key):
             bucket_name,
             s3_key,
             ExtraArgs={
-                "Metadata": {
-                    "md5": local_md5
+                'Metadata': {
+                    'md5': local_md5
                 }
             }
         )
 
-        print("File uploaded successfully.")
+        print('File uploaded successfully.')
 
     except FileNotFoundError:
-        print(f"File {file_path} not found.")
+        print(f'File {file_path} not found.')
 
     except S3UploadFailedError as e:
-        print(f"S3 Upload Failed: {e}")
+        print(f'S3 Upload Failed: {e}')
 
     except ClientError as e:
-        print(f"AWS ClientError: {e}")
+        print(f'AWS ClientError: {e}')
 
     except Exception as e:
-        print(f"Unexpected error: {e}")
-
+        print(f'Unexpected error: {e}')
 
 def calculate_md5(file_path):
+    """Calculate MD5 hash of a file.
+
+    Args:
+        file_path (str): The path to the file for which to calculate the MD5 hash.
+
+    Returns:
+        str: The MD5 hash of the file.
+    """
     hash_md5 = hashlib.md5()
 
-    with open(file_path, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
+    with open(file_path, 'rb') as f:
+        for chunk in iter(lambda: f.read(4096), b''):
             hash_md5.update(chunk)
 
     return hash_md5.hexdigest()
